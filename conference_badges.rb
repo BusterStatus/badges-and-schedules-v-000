e2c880bd21ed5a7839245e7f7e5016fbe498dbc1
matthew.serre <matthew.serre@gmail.com>
2018-10-25 01:39:49 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ end
 
 def assign_rooms(array)
   new_array = []
-  array.each_with_index { |name,number| new_array << "Hello, " + name + "! You'll be assigned to room " + (number + 1) + "!" }
+  array.each_with_index { |name,number| new_array << "Hello, " + name + "! You'll be assigned to room " + (number.to_i+ 1) + "!" }
   new_array
 end
 
