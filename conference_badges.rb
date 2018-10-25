dec2655a1259f03c065be2039a28620e25d0cc98
matthew.serre <matthew.serre@gmail.com>
2018-10-25 01:30:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,4 +9,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index {
+  array.each_with_index {|name, number| "Hello, " + name + "! You'll be assigned to room " + number +"!"
+end
